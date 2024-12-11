5842177cf347e0a20cb2945346da39eb4ca58f76
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-12-10 21:32:01 -0800

    merge(template): Merge svelte supabase template
    Merging the server authentication components from a template repo at: https://github.com/engageintellect/sveltekit-supabase.git

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+  
   import { dev } from '$app/environment';
   import Dashboard from "@/lib/components/Dashboard.svelte";
+  import UpdateProfile from '$lib/components/UpdateProfile.svelte';
+  
+  let data = $props();
   dev && console.log(`🛠️ In Dev Mode`);
+	// Reactive statement to update avatar when data changes
+	let userProfile = $state(data?.userProfile || '');
+	let avatar = $state(data?.user?.user_metadata?.avatar_url || '');
 </script>
 
-<Dashboard />
+{#if data?.userProfile}
+	<UpdateProfile {userProfile} {avatar} />
+{:else}
+  <Dashboard />
+{/if}/>
