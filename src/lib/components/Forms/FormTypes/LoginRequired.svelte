28a86ecfbf0a13df30019c420640d733914d826d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-12-23 01:03:47 -0800

    new(login): New login components for Forgot Password and Redirecting

---
+++ src/lib/components/Forms/FormTypes/LoginRequired.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Button from "../../ui/button/button.svelte";
+  import Card from "../../ui/card/card.svelte";
+  import FormContainer from "../Layouts/FormContainer.svelte";
+</script>
+
+<FormContainer>
+  <Card>
+    <div class="flex flex-col justify-center items-center h-full w-100 xl:min-w-72 py-[50%]">
+      <p>Please login before posting</p>
+      <Button class="my-5" href="/auth/login">Login</Button>
+      <p>
+        Need an account? 
+        <a href="/auth/signup" class="hover:text-sky-700">Click Here</a>
+      </p>
+    </div>
+  </Card>
+</FormContainer>
--- src/lib/components/Forms/FormTypes/index.ts
+++ src/lib/components/Forms/FormTypes/index.ts
@@ -1,3 +1,4 @@
-export { default as LoginForm} from "./LoginForm.svelte";
+export { default as LoginForm } from "./LoginForm.svelte";
 export { default as ReportForm } from "./ReportForm.svelte";
 export { default as SignUpForm } from "./SignUpForm.svelte";
+export { default as LoginRequired } from "./LoginRequired.svelte"
