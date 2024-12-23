d52fb3f1e00cac738af61e3f66c653bc30082b9d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-12-23 01:13:37 -0800

    chore(misc): Add misc left over files from large commit batch

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,48 +1,64 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createServerClient } from '@supabase/ssr'
-import type { Handle } from '@sveltejs/kit'
+import { dev } from "$app/environment";
+import {
+  PUBLIC_LOCAL_SUPABASE_ANON_KEY,
+  PUBLIC_LOCAL_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+} from "$env/static/public";
+import { createServerClient } from "@supabase/ssr";
+import type { Handle } from "@sveltejs/kit";
 
+// Handle dynamic assignment
+const [SUPABASE_URL, SUPABASE_ANON_KEY] = dev ? 
+  [PUBLIC_LOCAL_SUPABASE_URL, PUBLIC_LOCAL_SUPABASE_ANON_KEY] : 
+  [PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY];
 // Request middleware
 export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-    cookies: {
-      getAll() {
-        return event.cookies.getAll()
-      },
-      setAll(cookiesToSet) {
-        /**
-         * Note: You have to add the `path` variable to the
-         * set and remove method due to sveltekit's cookie API
-         * requiring this to be set, setting the path to an empty string
-         * will replicate previous/standard behavior (https://kit.svelte.dev/docs/types#public-types-cookies)
-         */
-        cookiesToSet.forEach(({ name, value, options }) =>
-          event.cookies.set(name, value, { ...options, path: '/' })
-        )
+  event.locals.supabase = createServerClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return event.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          /**
+           * Note: You have to add the `path` variable to the
+           * set and remove method due to sveltekit's cookie API
+           * requiring this to be set, setting the path to an empty string
+           * will replicate previous/standard behavior (https://kit.svelte.dev/docs/types#public-types-cookies)
+           */
+          cookiesToSet.forEach(({ name, value, options }) =>
+            event.cookies.set(name, value, { ...options, path: "/" })
+          );
+        },
       },
     },
-  })
+  );
   /**
    * Unlike `supabase.auth.getSession()`, which returns the session _without_
    * validating the JWT, this function also calls `getUser()` to validate the
    * JWT before returning the session.
    */
+  // NOTE: This method may cause warnings in the console due to being a wrapper for an unsafe method
   event.locals.safeGetSession = async () => {
     // Retrieve the session for this request
     const { data: { session } } = await event.locals.supabase.auth.getSession();
     // If no valid session return nothing
     if (!session) return { session: null, user: null };
     // Destructure user and error
-    const { data: { user }, error } = await event.locals.supabase.auth.getUser();
+    const { data: { user }, error } = await event.locals.supabase.auth
+      .getUser();
     // If JWT validation has failed
     if (error) return { session: null, user: null };
     // Return our supabase session and user objects
     return { session, user };
-  }
+  };
 
   return resolve(event, {
     filterSerializedResponseHeaders(name) {
-      return name === 'content-range' || name === 'x-supabase-api-version'
+      return name === "content-range" || name === "x-supabase-api-version";
     },
-  })
-}
+  });
+};
