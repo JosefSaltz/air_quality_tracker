52fec43fc4a7897290168a9227193eea8b6c340d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-12-23 00:57:23 -0800

    feat(navbar): Add navbar to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import "@/app.css";
 	import "@/app.css";
+  import Navbar from "@/lib/components/Navbar/Navbar.svelte"
   import { invalidate } from '$app/navigation'
   import { onMount } from 'svelte'
 
@@ -17,7 +18,7 @@
     return () => data.subscription.unsubscribe()
   })
 </script>
-
+<Navbar/>
 <div class="flex justify-center h-full">
   <div class="flex flex-col justify-center w-full h-full items-center">
       {@render children()}
