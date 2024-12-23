2cce13bc4a9419df375f2bfcc2f88258bf5f7ff9
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-12-23 00:50:10 -0800

    update(db): Add drizzle initalization
    profile table is now profiles

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
-import { DATABASE_URL } from "$env/static/private";
+import { DATABASE_URL, LOCAL_DATABASE_URL } from "$env/static/private";
 
-const client = postgres(DATABASE_URL);
+console.log(`env:`, import.meta.env.PROD);
+const db_url = import.meta.env.PROD ? DATABASE_URL : LOCAL_DATABASE_URL;
+const client = postgres(db_url);
 export const db = drizzle(client, { schema });
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
-export const profileTable = pgTable("profile", {
+export const profileTable = pgTable("profiles", {
   id: uuid("id").primaryKey(),
   firstName: text("first_name").notNull(),
   lastName: text("last_name").notNull(),
