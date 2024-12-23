ca9c19395e0ddd1e1ff02eccc5420584d63c0d9d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-12-23 00:53:54 -0800

    feat(supabase): Dev mode dynamic env init

--- src/lib/utils/client.ts
+++ src/lib/utils/client.ts
@@ -1,10 +1,20 @@
 import { createClient } from "@supabase/supabase-js"
 
-const { 
-  VITE_SUPABASE_PROJECT_URL: SUPABASE_PROJECT_URL, 
-  VITE_SUPABASE_ANON_KEY: SUPABASE_ANON_KEY 
-} = import.meta.env;
+import { 
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_LOCAL_SUPABASE_ANON_KEY,
+  PUBLIC_LOCAL_SUPABASE_URL, 
+} from '$env/static/public';
 
+const inProd = import.meta.env.PROD;
+// Dev environment logic
+if(!inProd) console.info('Loading dev environment...');
+
+console.log(`env:`, import.meta.env.PROD);
+const SUPABASE_PROJECT_URL = inProd ? PUBLIC_SUPABASE_URL : PUBLIC_LOCAL_SUPABASE_URL
+const SUPABASE_ANON_KEY = inProd ? PUBLIC_SUPABASE_ANON_KEY : PUBLIC_LOCAL_SUPABASE_ANON_KEY;
+// Null Check
 if(!SUPABASE_PROJECT_URL || !SUPABASE_ANON_KEY) console.error("No ENV variables");
 
 const supabase = createClient(SUPABASE_PROJECT_URL!, SUPABASE_ANON_KEY!);
