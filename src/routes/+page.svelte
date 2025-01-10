f9289728b042c897a715c9cfb25936938853bd92
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-10 15:23:37 -0800

    fix(types): Correct page load data type

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import { dev } from '$app/environment';
   import Dashboard from "@/lib/components/Dashboard/Dashboard.svelte";
+  import type { PageData } from './$types';
   
-  let data = $props();
-  dev && console.log(`🛠️ In Dev Mode`);
-	// Reactive statement to update avatar when data changes
-	let userProfile = $state(data?.userProfile || '');
+  let { data }: { data: PageData } = $props();
+  dev && console.log(`🛠️ In Dev Mode`);	
 </script>
 
 <Dashboard markers={data.markers} />
