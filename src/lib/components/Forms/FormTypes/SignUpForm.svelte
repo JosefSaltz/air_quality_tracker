c659a912f4110b8d705b3b1cb1bdc982e72c620e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-09 21:59:02 -0800

    update(SignUp): Add logic and bind sign up form inputs

--- src/lib/components/Forms/FormTypes/SignUpForm.svelte
+++ src/lib/components/Forms/FormTypes/SignUpForm.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
   import FormContainer from "@/lib/components/Forms/Layouts/FormContainer.svelte";
+  import { z } from 'zod';
+  const emailSchema = z.string().email();
+  let confirm_pw = $state();
+  let email = $state();
+  let password = $state();
+  let valid_email = $derived(emailSchema.safeParse(email).success);
+  let verified_pw = $derived(password && confirm_pw && password === confirm_pw);
+  let disable_submit = $derived(!(verified_pw && valid_email));
 </script>
 
 <FormContainer>
@@ -15,24 +23,39 @@
         <div>
           <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
           <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+            <input type="email" name="email" id="email" bind:value={email} autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
           </div>
+          {#if email}
+            {#if valid_email}
+              <p class="text-green-500">Valid email</p>
+            {:else}
+              <p class="text-red-500">Not a valid email address</p>
+            {/if}  
+          {/if}
         </div>
 
         <div>
           <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
           <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+            <input type="password" name="password" bind:value={password} id="password" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
           </div>
 
           <label for="confirm" class="block text-sm/6 font-medium text-gray-900">Verify Password</label>
           <div class="mt-2">
-            <input type="password" name="confirm" id="confirm" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+            <input type="password" name="confirm" bind:value={confirm_pw} id="confirm" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
           </div>
         </div>
 
+        {#if password && confirm_pw}
+          {#if verified_pw}
+            <p class="text-green-500">Passwords match</p>
+          {:else}
+            <p class="text-red-500">Passwords do not match</p>
+          {/if}  
+        {/if}
+
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-stone-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg--500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline--600">Sign up</button>
+          <button type="submit" class={`flex w-full justify-center rounded-md ${disable_submit ? 'bg-stone-300' : 'bg-stone-600'} px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg--500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline--600`} disabled={disable_submit}>Sign up</button>
         </div>
       </form>
 
