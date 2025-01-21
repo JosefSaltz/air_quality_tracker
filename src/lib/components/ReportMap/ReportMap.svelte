da1abae106147044aa6beb052518a008e5837baa
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-20 21:01:31 -0800

    new(reportMap): Add marker generation to report map

--- src/lib/components/ReportMap/ReportMap.svelte
+++ src/lib/components/ReportMap/ReportMap.svelte
@@ -7,19 +7,20 @@
   import { onMount } from "svelte";
   import "leaflet/dist/leaflet.css";
   import fetchGeolocation from "$lib/utils/fetchGeolocation";
-  import type { Map } from "leaflet";
+  import type { LatLngTuple, Map } from "leaflet";
   import MapSkeleton from "$components/ReportMap/MapSkeleton.svelte";
   import { dev } from "$app/environment";
-
+  import type {} from "leaflet"
+  import type { Tables } from "$root/database.types";
   let { markers } = $props(); 
   let container: undefined | Element;
-  let leaflet;
+  let leaflet: typeof import('leaflet') | undefined;
   let lMap:  undefined | Map = $state();
   const initialView = {
     latitude: 38.10105120505375,
     longitude: -122.25144198851173
   }
-  let currentGeolocation: GeoCoords = $state(initialView);
+  let currentGeolocation = $state(initialView);
   // Change this to null to query the user's device
   // Binds coordinates to Vallejo, CA absolutely
   // Unsure about how to handle geolocation outside of Vallejo, CA as extraneous data
@@ -27,13 +28,17 @@
   // Semantic alias
   const devDefault = initialView;
 
+  const chooseLocationSource = (retrievedGeo: GeoCoords) => dev ? devDefault : retrievedGeo; 
 
   const fetchGeoAndUpdate = async () => {
     const coords = await fetchGeolocation();
+    // current Geolocation will be initial if in dev mode
     currentGeolocation = chooseLocationSource(coords);
-    lMap && lMap.setView([coords.latitude, coords.longitude])
+    // Check that lMap exists and then set it's view to the new coords
+    lMap && lMap.setView([currentGeolocation.latitude, currentGeolocation.longitude])
   }
-  const chooseLocationSource = (retrievedGeo: GeoCoords) => dev ? devDefault : retrievedGeo; 
+  
+  
 
   onMount(async () => {
     try {
@@ -44,6 +49,14 @@
       const { latitude: x, longitude: y } = currentGeolocation;
       // Dynamically import the leaflet library to resolve CSR requirements (window global)
       leaflet = await import("leaflet");
+
+      const generateMarkers = (lMap: Map) => {
+        markers.forEach((marker: Tables<'reports'>) => {
+          const markerCoords = [ marker.latitude, marker.longitude ] satisfies LatLngTuple;
+          if(!leaflet) return console.error('Leaflet is now initialized!')
+          leaflet.marker(markerCoords).addTo(lMap);
+        })
+      }
       console.log(`loaded leaflet`)
       // Initialize the Leaflet map object bound to the element with #map
       lMap = leaflet.map("map").setView([x, y], 13);
@@ -51,6 +64,7 @@
       leaflet
         .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {})
         .addTo(lMap);
+      generateMarkers(lMap);
       // Set the watermark attribution
       lMap.attributionControl.setPrefix("github.com/JosefSaltz");
       console.log("Done!");
