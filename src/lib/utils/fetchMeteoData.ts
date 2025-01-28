0e17e532f52ece14090cdbffb77d965989a0a217
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-28 00:20:54 -0800

    update(fetchMeteoData): Update to use current data instead of minutely
    This might need to get refactored at some point if we move to specified date range data

--- src/lib/utils/fetchMeteoData.ts
+++ src/lib/utils/fetchMeteoData.ts
@@ -1,6 +1,4 @@
 import { fetchWeatherApi } from 'openmeteo';
-import type { GeoCoords } from '../components/ReportMap/ReportMap.svelte';
-
 
 export async function fetchMeteoData ({ latitude , longitude }: { latitude: number | null, longitude: number | null}) {
   if(!latitude || !longitude) return console.error('Was not able to fetch Meteo Data from lack of geo data');
@@ -8,11 +6,11 @@ export async function fetchMeteoData ({ latitude , longitude }: { latitude: numb
   const params = {
     latitude,
     longitude,
-    "minutely_15": ["temperature_2m", "wind_speed_10m", "wind_direction_10m"],
+    "current": ["temperature_2m", "relative_humidity_2m", "precipitation", "wind_speed_10m", "wind_direction_10m"],
     "temperature_unit": "fahrenheit",
     "wind_speed_unit": "kn",
     "precipitation_unit": "inch",
-    "forecast_days": 1
+    "forecast_days": 1,
   };
   const url = "https://api.open-meteo.com/v1/forecast";
   const responses = await fetchWeatherApi(url, params);
@@ -29,41 +27,18 @@ export async function fetchMeteoData ({ latitude , longitude }: { latitude: numb
   const timezoneAbbreviation = response.timezoneAbbreviation();
   //const latitude = response.latitude();
   //const longitude = response.longitude();
-
-  const minutely15 = response.minutely15()!;
-
+  const current = response.current()!;
   // Note: The order of weather variables in the URL query and the indices below need to match!
   const weatherData = {
-
-    minutely15: {
-      time: range(Number(minutely15.time()), Number(minutely15.timeEnd()), minutely15.interval()).map(
-        (t) => new Date((t + utcOffsetSeconds) * 1000)
-      ),
-      temperature2m: minutely15.variables(0)!.valuesArray()!,
-      windSpeed10m: minutely15.variables(1)!.valuesArray()!,
-      windDirection10m: minutely15.variables(2)!.valuesArray()!,
-    },
-
+    current: {
+      time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+      temperature2m: current.variables(0)!.value(),
+      relativeHumidity2m: current.variables(1)!.value(),
+      precipitation: current.variables(2)!.value(),
+      windSpeed10m: current.variables(3)!.value(),
+      windDirection10m: current.variables(4)!.value(),
+    }
   };
-
-  // `weatherData` now contains a simple structure with arrays for datetime and weather data
-  for (let i = 0; i < weatherData.minutely15.time.length; i++) {
-    console.log(
-      weatherData.minutely15.time[i].toISOString(),
-      weatherData.minutely15.temperature2m[i],
-      weatherData.minutely15.windSpeed10m[i],
-      weatherData.minutely15.windDirection10m[i]
-    );
-  }
 
   return weatherData;
 }
-
-
-
-
-
-
-
-
-
