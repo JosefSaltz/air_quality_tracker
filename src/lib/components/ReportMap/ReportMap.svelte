51497c6f2c7917750c9319a38b5ebb2363a452b9
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-29 03:22:40 -0800

    refactor(ReportMap): Uses more straightforward marker edits

--- src/lib/components/ReportMap/ReportMap.svelte
+++ src/lib/components/ReportMap/ReportMap.svelte
@@ -12,13 +12,13 @@
   import type { PageProps } from "../../../routes/$types";
   import type { LatLngTuple, Map } from "leaflet";
   import Button from "../ui/button/button.svelte";
-  import addCenterMarker from "../Leaflet/leaflet.centermarker";
   
   type Props = {
     markers: PageProps["data"]["markers"],
     currentGeolocation: GeoCoords,
     initialView: GeoCoords
   }
+
   let { markers, currentGeolocation = $bindable(), initialView }: Props = $props(); 
   let lMap:  undefined | Map = $state();
   let L: typeof import('leaflet') | undefined;
@@ -38,48 +38,49 @@
   }
   // CSR logic
   onMount(async () => {
-    try {
-      // Define marker generation
-      const generateMarkers = (lMap: Map) => {
+    // Define marker generation
+    const generateMarkers = (lMap: Map) => {
         if (!markers) return null;
-
+        // Iterate through marker data and create a new marker to be placed on the leaflet map
         return markers.map((marker) => {
           const markerCoords = [ marker.latitude, marker.longitude ] satisfies LatLngTuple;
           if(!L) return console.error('Leaflet is not initialized!')
           L.marker(markerCoords).addTo(lMap);
         })
       }
-      // Loading Start
-      console.log(`Loading map...`);
-      // Async fetch up to date geo coordinates and update the state
-      fetchGeoAndUpdate();
-      // Destructure current geo values
-      const { latitude: x, longitude: y } = currentGeolocation;
-      // Dynamically import the leaflet library to resolve CSR requirements (window global)
-      L = await import("leaflet");
-      // Initialize the Leaflet map object bound to the element with #map
-      lMap = L.map("map").setView([x, y], 13);
-      // Set OpenStreetMap as the tile layer and add to map object
-      L
-        .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {})
-        .addTo(lMap);
-      const centerMarkerFactory = addCenterMarker(window);
-      const selectMarker = L.centerMarker(lMap);
-      selectMark
-      console.log(typeof selectMarker);
-      selectMarker.on("newposition", () => { const latLang = selectMarker.getLatLng(); console.log(latLang); currentGeolocation = latLang})
-      // Add all the markers to the map iteratively
-      generateMarkers(lMap);
-      // Set the watermark attribution
-      lMap.attributionControl.setPrefix("github.com/JosefSaltz");
-      // Log Completion
-      console.log("Done!");
-    } catch (err) {
-      console.error(
-        `Something went wrong trying to get the geolocation data`,
-        err
-      );
-    }
+    // Grab and update Geo coords
+    try { fetchGeoAndUpdate() }
+    catch (err) { console.error(`Something went wrong trying to get the geolocation data`, err); }
+    // Loading Start
+    console.log(`Loading map...`);
+    // Destructure current geo state values
+    const { latitude: x, longitude: y } = currentGeolocation;
+    // Dynamically import the leaflet library to resolve CSR requirements (window global req)
+    L = await import("leaflet");
+    // Initialize the Leaflet map object bound to the element with #map id
+    lMap = L.map("map").setView([x, y], 13);
+    // Set OpenStreetMap as the tile layer and add to map object
+    L
+      .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {})
+      .addTo(lMap);
+    // Create the centered selector marker
+    const selectorMarker = new L.Marker(lMap.getCenter()).addTo(lMap);
+    // Map Event Listeners
+    // Update the marker position to center on drag
+    lMap.on("drag", () => { selectorMarker.setLatLng(lMap.getCenter())})
+    // Update state on end of drag
+    lMap.on("dragend", () => { 
+      // Destructure
+      const { lat, lng } = selectorMarker.getLatLng(); 
+      // Fit values to keys 
+      currentGeolocation = { latitude: Number(lat), longitude: Number(lng)}
+    });
+    // Add all the previously reported markers to the map
+    generateMarkers(lMap);
+    // Set the watermark attribution
+    lMap.attributionControl.setPrefix("github.com/JosefSaltz");
+    // Log Completion
+    console.log("Done!"); 
   });
   // Effect to handle dynamic resizing
   $effect(() => {
@@ -99,15 +100,15 @@
 		})
 </script>
 <!-- Leafly attachment node -->
-<Button class="absolute my-4">Report</Button>
-<div id="map" bind:this={container} class={`w-full h-full border`}>
-  {#if !lMap}
-    <MapSkeleton />
-  {/if}
-  
+<div id="map-container" class="w-full h-full border" >
+  <Button class="absolute my-4">Report</Button>
+  <div id="map" bind:this={container} class={`w-full h-full`}>
+    {#if !lMap}
+      <MapSkeleton />
+    {/if}
+  </div>
 </div>
 
-<!-- </Card> -->
 <style>
   #map {
     border-right: none;
