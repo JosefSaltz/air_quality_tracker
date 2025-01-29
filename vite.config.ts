ccfc24ccb8b4e06ffed2c6093a5edc1e4200d49a
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-28 21:59:14 -0800

    feat(svelte-enhanced-image): Add Sveltekit's enhanced image plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		sveltekit(), 
+		enhancedImages(),
+		sveltekit() 
 	],
 	server: {
 		port: 3000
