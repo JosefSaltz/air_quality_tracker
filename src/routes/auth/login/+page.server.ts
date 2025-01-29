48925f6eef19c09be651fb5d0a3cb3639ca7cdfb
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-28 21:59:53 -0800

    new(images): Add favicons and related logo assets

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -1,16 +1,30 @@
-import { redirect, type Actions } from "@sveltejs/kit";
+import type { AuthError } from "@supabase/supabase-js";
+import { redirect, type Actions, fail } from "@sveltejs/kit";
 
 export const actions = {
-  login: async ({ request, locals: { supabase } }) => {
+  login: async ({ request, locals: { supabase }, params }) => {
+    const handleError = (error: AuthError | null) => {
+      if (error) {
+        console.error(error)
+        return fail(401, { error })
+      } 
+      redirect(303, '/')
+    }
     const formData = await request.formData()
     const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    const { error } = await supabase.auth.signInWithPassword({ email, password })
-    
-    if (error) {
-      console.error(error)
-      redirect(303, '/auth/error')
-    } 
-    redirect(303, '/')
+    const password = formData.get('password') as string;
+    let hadError = null;
+    // Social OAuth
+    if(params?.sso && params?.provider === "google") {
+      const { error } = await supabase.auth.signInWithOAuth({ provider: 'google'})
+      hadError = error;
+    }
+    // Default Email Password
+    else { 
+      const { error } = await supabase.auth.signInWithPassword({ email, password }) 
+      hadError = error;
+    }
+    // Error handling
+    if(hadError) return handleError(hadError);
   },
 } satisfies Actions;
