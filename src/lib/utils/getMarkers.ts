9cb413b187cb86f7313702ccf53d53e62c5b1b6b
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-01-31 04:02:04 -0800

    update(getMarkers): Now more restrictive in select query
    Also returns the straight data payload now

--- src/lib/utils/getMarkers.ts
+++ src/lib/utils/getMarkers.ts
@@ -1,11 +1,11 @@
 import supabase from "@/lib/utils/client";
 
 export async function getMarkers() {
-  const markerData = await supabase.from("reports").select();
-  const { error } = markerData;
+  const markers = await supabase.from("reports").select("id, created_at, description, humidity, latitude, longitude, strength, temperature_f, wind_direction, wind_speed_kn");
+  const { error } = markers;
   if(error) {
     console.error(`Something went wrong trying to retrieve marker data`, error.message);
     return null;
   }
-  return markerData;
+  return markers.data;
 }
