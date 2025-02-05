7213e536d915f89afa743376ab91e4d5b1ec4413
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-05 08:01:08 -0800

    update(styles): Tweak responsiveness

--- src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
+++ src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
@@ -11,12 +11,12 @@
 
 <div class="sm:mx-auto sm:w-full sm:max-w-[480px]">
   <div class="sm:mx-auto w-full xl:w-auto sm:max-w-md flex flex-col items-center">
-    <enhanced:img src="../../../../assets/logo.png" class="h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
-    <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+    <enhanced:img src="../../../../assets/logo.png" class=" h-28 md:h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
+    <h2 class="mt-3 md:mt-6 text-center text-lg md:text-2xl font-bold tracking-tight text-gray-900">
       PIITA Login
     </h2>
   </div>
-  <div class="bg-white px-6 py-12 sm:rounded-lg sm:px-12">
+  <div class="bg-white px-6 py-6 md:py-12 sm:rounded-lg sm:px-12">
     <form class="space-y-6" action="?/login" method="POST">
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">
