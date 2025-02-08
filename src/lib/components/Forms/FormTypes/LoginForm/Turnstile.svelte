2db9889e09e4358d85a983e54d1537343275efbe
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-08 06:58:10 -0800

    new(turnstile): Add client-side implementation

---
+++ src/lib/components/Forms/FormTypes/LoginForm/Turnstile.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { PUBLIC_TURNSTILE_KEY } from "$env/static/public";
+</script>
+
+<svelte:head>
+  <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+</svelte:head>
+
+<div
+  class="cf-turnstile"
+  data-sitekey={PUBLIC_TURNSTILE_KEY}
+  data-callback="javascriptCallback"
+></div>
