dcb96990a3fa59b471419c6e65fe383d8e381d1d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-08 04:11:32 -0800

    update(sentry): Disable in dev

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,12 +1,13 @@
 import * as Sentry from "@sentry/sveltekit";
-
+import { dev } from "$app/environment";
+// Only initialize sentry if we're not in development server
 Sentry.init({
   dsn: "https://18b27370300a72c50f62bfda6c9ba9a7@o4508782595866624.ingest.us.sentry.io/4508782597308416",
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
-
+  enabled: !dev,
   // Optional: Initialize Session Replay:
   integrations: [Sentry.replayIntegration()],
   replaysSessionSampleRate: 0.1,
