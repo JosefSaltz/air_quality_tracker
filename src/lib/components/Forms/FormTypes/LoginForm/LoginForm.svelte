616157425649db1a4b190b2358f9a7b6a4f3f94c
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-08 06:59:09 -0800

    fix(login): Add enhance property to login form

--- src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
+++ src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
-  import logo from "$lib/assets/logo.png"
-  // const handleSubmission = (e) =>{
-  //   if(!FormData.get("email") || !FormData.get("password"))
-  // }
+  import logo from "$lib/assets/logo.png";
+  import { PUBLIC_TURNSTILE_KEY } from "$env/static/public";
   const disabled = true;
-
 </script>
 
 <div class="sm:mx-auto sm:w-full sm:max-w-[480px]">
   <div class="sm:mx-auto w-full xl:w-auto sm:max-w-md flex flex-col items-center">
-    <enhanced:img src="../../../../assets/logo.png" class=" h-28 md:h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
+    <enhance:img src="../../../../assets/logo.png" class=" h-28 md:h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
     <h2 class="mt-3 md:mt-6 text-center text-lg md:text-2xl font-bold tracking-tight text-gray-900">
       PIITA Login
     </h2>
   </div>
   <div class="bg-white px-6 py-6 md:py-12 sm:rounded-lg sm:px-12">
-    <form class="space-y-6" action="?/login" method="POST">
+    <enhance:form class="space-y-6" action="?/login" method="POST">
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">
           Email address
@@ -78,6 +75,8 @@
             <span class="text-sm/6 font-semibold">Google</span>
           </button>
 
+
+
           <!-- <a href="/auth/login?sso=true&provider=apple" class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent">
             <svg class="size-5 fill-[#24292F]" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
               <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
@@ -86,6 +85,7 @@
           </a> -->
         </div>
       </div>
+      <div class="cf-turnstile" data-sitekey={PUBLIC_TURNSTILE_KEY}></div>
     </form>
   </div>
 
