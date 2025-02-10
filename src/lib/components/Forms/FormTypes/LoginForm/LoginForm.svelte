7d0b6fdf82330f2fbf33d59aa65158d7e693cfea
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-10 03:43:13 -0800

    fix(loginform): Undo changing enhanced to enhance

--- src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
+++ src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
@@ -7,7 +7,7 @@
 
 <div class="sm:mx-auto sm:w-full sm:max-w-[480px]">
   <div class="sm:mx-auto w-full xl:w-auto sm:max-w-md flex flex-col items-center">
-    <enhance:img src="../../../../assets/logo.png" class=" h-28 md:h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
+    <enhanced:img src="../../../../assets/logo.png" class=" h-28 md:h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
     <h2 class="mt-3 md:mt-6 text-center text-lg md:text-2xl font-bold tracking-tight text-gray-900">
       PIITA Login
     </h2>
