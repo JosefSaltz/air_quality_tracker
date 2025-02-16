b42c61844b9aad7896a52ad06162be9a9fdb1e7a
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-15 19:09:42 -0800

    debug(ci/cd): peek env during deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
 import deno from "@deno/vite-plugin";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { sentrySvelteKit } from "@sentry/sveltekit";
+import { mode } from "mode-watcher";
 
-export default defineConfig({
-	build: {
-		assetsInlineLimit: 0
-	},
-	plugins: [
-		deno(),
-		sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: "piita",
-        project: "javascript-sveltekit",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        sourcemaps: {
-          assets: ["./build/*/**/*"],
-          ignore: ["**/build/client/**/*"],
-          filesToDeleteAfterUpload: ["./build/**/*.map"],
-        },
-      },
-    }),
-		sveltekit(),
-		enhancedImages(),
-	],
-	resolve: {
-		alias: {
-			"zlib": "node:zlib"
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	console.log(env)
+	return ({
+		build: {
+			assetsInlineLimit: 0
+		},
+		plugins: [
+			deno(),
+			sentrySvelteKit({
+				sourceMapsUploadOptions: {
+					org: "piita",
+					project: "javascript-sveltekit",
+					authToken: process.env.SENTRY_AUTH_TOKEN,
+					sourcemaps: {
+						assets: ["./build/*/**/*"],
+						ignore: ["**/build/client/**/*"],
+						filesToDeleteAfterUpload: ["./build/**/*.map"],
+					},
+				},
+			}),
+			sveltekit(),
+			enhancedImages(),
+		],
+		resolve: {
+			alias: {
+				"zlib": "node:zlib"
+			}
+		},
+		server: {
+			port: 3000
 		}
-	},
-	server: {
-		port: 3000
-	}
+	})
 });
