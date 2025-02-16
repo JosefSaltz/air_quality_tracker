c70c05ae3ec2281c71d2c90d3998da9f1f6b7d64
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-15 19:11:19 -0800

    fix(vite): Remove broken import

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig, loadEnv } from 'vite';
 import { sentrySvelteKit } from "@sentry/sveltekit";
-import { mode } from "mode-watcher";
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
