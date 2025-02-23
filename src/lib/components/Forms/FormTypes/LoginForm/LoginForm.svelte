a19cb8ef532616b37f8d5f64ce032e81251d164d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-02-23 06:30:26 -0800

    fix(LoginForm): Vertically center the login from input better

--- src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
+++ src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
@@ -5,7 +5,7 @@
   const disabled = true;
 </script>
 
-<div class="sm:mx-auto sm:w-full sm:max-w-[480px]">
+<div id="login-form-container" class="h-full flex flex-col justify-center sm:mx-auto sm:w-full sm:max-w-[480px]">
   <div class="sm:mx-auto w-full xl:w-auto sm:max-w-md flex flex-col items-center">
     <enhanced:img src="../../../../assets/logo.png" class=" h-28 md:h-36 xl:h-64 w-auto rounded-xl" alt="PIITA Logo" />
     <h2 class="mt-3 md:mt-6 text-center text-lg md:text-2xl font-bold tracking-tight text-gray-900">
