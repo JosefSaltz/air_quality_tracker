4062f44408b622f211f4b32723f348e21221b75e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-24 15:28:01 -0700

    update(Turnstile):  Use Svelte-Put's implementation
    This has rune support and resolves CSR issues of the token going stale between navigation

---
+++ src/lib/components/Forms/Turnstile.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { turnstile } from '@svelte-put/cloudflare-turnstile';
+  import { PUBLIC_TURNSTILE_KEY } from "$env/static/public";
+  type Props = { 
+    callback: (response: Response) => void, 
+    className?: string, 
+    wasChecked: boolean 
+  };
+  let { callback, className, wasChecked = $bindable() }: Props = $props();
+</script>
+
+<div
+  use:turnstile
+  id="turnstile-widget"
+  class={`cf-turnstile ${className}`}
+  turnstile-sitekey={PUBLIC_TURNSTILE_KEY}
+  turnstile-theme="light"
+  onturnstile={(e) => { wasChecked = true } }
+></div>
