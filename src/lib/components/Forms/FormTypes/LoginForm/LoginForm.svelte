43ff22d80b4490631977e62f9b17dda0d847052b
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-24 22:23:28 -0700

    update(LoginForm): Re-enable email auth inputs

--- src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
+++ src/lib/components/Forms/FormTypes/LoginForm/LoginForm.svelte
@@ -37,14 +37,14 @@
           Email address
         </label>
         <div class="mt-2">
-          <input disabled={disabled} type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+          <input  type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
         </div>
       </div>
       <!-- Password Input -->
       <div>
         <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
         <div class="mt-2">
-          <input disabled={disabled} type="password" name="password" id="password" autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+          <input type="password" name="password" id="password" autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
         </div>
       </div>
       <!-- Remember Me Check Box -->
