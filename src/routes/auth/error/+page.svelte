b11f64bbbc46513a4643989f64e93eb22dc249be
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-26 16:47:25 -0700

    update(error page): Format content and add redirection timer

--- src/routes/auth/error/+page.svelte
+++ src/routes/auth/error/+page.svelte
@@ -1,1 +1,16 @@
-<p>Oops something went wrong</p>
+<script lang="ts">
+  import { redirect } from "@sveltejs/kit";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    // Redirection Timer
+    setTimeout(() => {
+      redirect(303, "/auth/login"); 
+    }, 500)
+  })
+</script>
+
+<div id="error-container" class="w-full h-full flex flex-col justify-center items-center">
+  <p class="text-3xl">⚠️ Error During Auth Request</p>
+  <p>Redirecting...</p>
+</div>
