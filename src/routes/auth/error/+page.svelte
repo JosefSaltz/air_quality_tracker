9e6bf20b899e781609d4bd649529f9335d5c998a
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-26 18:20:39 -0700

    update(auth error route): Auth error page now uses a timer to redirect the user back to login

--- src/routes/auth/error/+page.svelte
+++ src/routes/auth/error/+page.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
-  import { redirect } from "@sveltejs/kit";
-  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  // Set initial state of 5 seconds
+  let time = $state(5000);
 
-  onMount(() => {
-    // Redirection Timer
-    setTimeout(() => {
-      redirect(303, "/auth/login"); 
-    }, 500)
+  $effect(() => {
+    let timer: NodeJS.Timeout;
+    // Set a one second timer to update state 
+    if(time > 0) {
+      timer = setTimeout(() => {
+        time = time - 1000;
+      }, 1000)
+    }
+    // When out of time redirect to the login page
+    else {
+      goto("/auth/login")
+    }
+    // Clean up function
+    return () => clearTimeout(timer);
   })
 </script>
 
 <div id="error-container" class="w-full h-full flex flex-col justify-center items-center">
-  <p class="text-3xl">⚠️ Error During Auth Request</p>
-  <p>Redirecting...</p>
+  <p class="text-3xl">⚠️ Error During Authentication Request</p>
+  <p>Redirecting{ time ? ` in ${time / 1000}s` : ``}...</p>
 </div>
