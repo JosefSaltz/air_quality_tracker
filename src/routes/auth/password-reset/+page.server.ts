8588564961715af6f97d0273eb69c6aa1bd9f77e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-29 17:39:15 -0700

    add(password reset): Various pages for resetting the user's password

---
+++ src/routes/auth/password-reset/+page.server.ts
@@ -0,0 +1,11 @@
+import { fail, type Actions } from "@sveltejs/kit";
+
+export const actions = {
+  "reset-password": async ({ request, locals: { supabase } }) => {
+    const formData = await request.formData();
+    const email = formData.get("email") as string;
+    if(!email) return fail(400)
+    const { error } = await supabase.auth.resetPasswordForEmail(email, { redirectTo: "/auth/update-password" });
+    if(error) return fail(500);
+  }
+} satisfies Actions
---
+++ src/routes/auth/password-reset/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import LogoPiita from "@/lib/components/LogoPiita.svelte";
+import { z } from "zod";
+  let email = $state(null);
+  const emailSchema = z.string().email();
+  let validEmail = $derived(emailSchema.safeParse(email).success);
+
+</script>
+
+<div id="password-reset-container" class="w-ful h-full flex flex-col justify-center items-center">
+   <!-- Logo and Header -->
+   <LogoPiita name="Reset Password"/>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+    <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+      <form class="space-y-6" action="?/reset-password" method="POST">
+        <div>
+          <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input type="email" name="email" id="email" bind:value={email} autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+          </div>
+        </div>
+        <!-- Email Validation Subtitle -->
+        {#if email}
+          {#if validEmail}
+            <p class="text-green-500">Valid Email</p>
+            {:else}
+            <p class="text-red-500">Invalid Email</p>
+          {/if}
+        {/if}
+
+        <div class="flex items-center justify-right">
+          <div class="text-sm/6">
+            <a href="/auth/login" class="font-semibold text--600 hover:text--500">🔙 to Login</a>
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" disabled={!validEmail} formaction="?/reset-password" class={`flex w-full justify-center rounded-md ${validEmail ? 'bg-purple-500' : 'bg-purple-200'} px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg--500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline--600`}>Send email</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
