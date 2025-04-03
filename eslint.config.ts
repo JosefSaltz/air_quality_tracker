8c7caaae0542f2a1a69dacc573c3f3190b15f0e9
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-03 14:55:01 -0700

    update(deps): Update deps and added various eslint deps

---
+++ eslint.config.ts
@@ -0,0 +1,42 @@
+import * as js from '@eslint/js';
+import * as svelte from 'eslint-plugin-svelte';
+import * as globals from 'globals';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node // Add this if you are using SvelteKit in non-SPA mode
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        // We recommend importing and specifying svelte.config.js.
+        // By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
+        // While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
+        // explicitly specifying it ensures better compatibility and functionality.
+        svelteConfig
+      }
+    }
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^\\$\\$(Props|Events|Slots|Generic)$"
+        }
+      ]
+    },
+  }
+]
