e4cb8013d25ac2a75af1ef10bf0962fbb23bfffa
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-15 18:32:10 -0700

    add(constant): Make the timeOptions map it's own constant

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const timeOptions = new Map([
+  ["today", { name: "Today", value: 1 }],
+  ['week', { name: "Week", value: 7 }],
+  ['month', { name: "Month", value: 30 }],
+  ['custom', { name: "Custom", value: false }]
+]);
