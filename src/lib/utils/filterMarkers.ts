553c62609c5212c247aadab3d53fbbb71bad9e86
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-15 18:32:47 -0700

    update(filterMarkers): Tweak naming

--- src/lib/utils/filterMarkers.ts
+++ src/lib/utils/filterMarkers.ts
@@ -1,18 +1,19 @@
 import type { PageProps } from "../../routes/$types";
 import { Temporal } from '@js-temporal/polyfill';
 
-export function filterMarkers(markers: Awaited<PageProps["data"]["markers"]>, days: number) { 
-  if(!markers) return null;
+export async function filterMarkers(markers: PageProps["data"]["markers"], days = 30) { 
+  const markerData = await markers;
+  if(!markerData) return null;
+  const timeZone = Temporal.Now.timeZoneId();
   // Get time for now
-  const now = Temporal.Now.zonedDateTimeISO();
+  const now = Temporal.Now.zonedDateTimeISO(timeZone);
+  // Determine the oldest date from now
+  const oldestLimit = now.subtract({ days });
   // Returns a filtered marker list between the supplier date range
-  return markers.filter(marker => {
+  return markerData.filter(marker => {
     // Get an instant of the marker's date
-    const markerDate = Temporal.Instant.from(marker.created_at);
-    // Determine the oldest date from now
-    const oldestLimit = now.subtract({ days });
+    const markerDate = Temporal.Instant.from(marker.created_at).toZonedDateTimeISO(timeZone);
     // Return true if markerDate is newer than the oldest allowed time and before now
     return markerDate >= oldestLimit && markerDate <= now;
   })
 }
-
