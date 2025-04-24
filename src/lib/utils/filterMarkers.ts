8182238288a82172996c9a093ed65a818646fab9
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-23 18:40:35 -0700

    new(filterMarkers): Search logic and Fuse implementation

--- src/lib/utils/filterMarkers.ts
+++ src/lib/utils/filterMarkers.ts
@@ -1,19 +1,47 @@
+import { page } from "$app/state";
+import { marker } from "leaflet";
 import type { PageProps } from "../../routes/$types";
 import { Temporal } from '@js-temporal/polyfill';
+import Fuse from 'fuse.js';
 
-export async function filterMarkers(markers: PageProps["data"]["markers"], days = 30) { 
-  const markerData = await markers;
-  if(!markerData) return null;
+type MarkersList = Awaited<PageProps["data"]["markers"]>
+type FilteredResults = ReturnType<typeof filterMarkersByDate | typeof filterMarkersByTerm>
+
+function filterMarkersByDate(markers: MarkersList, days = 30) { 
+  if(!markers) return null;
   const timeZone = Temporal.Now.timeZoneId();
   // Get time for now
   const now = Temporal.Now.zonedDateTimeISO(timeZone);
   // Determine the oldest date from now
   const oldestLimit = now.subtract({ days });
   // Returns a filtered marker list between the supplier date range
-  return markerData.filter(marker => {
+  return markers.filter(marker => {
     // Get an instant of the marker's date
     const markerDate = Temporal.Instant.from(marker.created_at).toZonedDateTimeISO(timeZone);
     // Return true if markerDate is newer than the oldest allowed time and before now
     return markerDate >= oldestLimit && markerDate <= now;
   })
 }
+
+function filterMarkersByTerm(markers: ReturnType<typeof filterMarkersByDate>, searchTerm?: string) {
+  // Null Guards
+  if(!markers) return [];
+  if(!searchTerm) return markers;
+  // Initialize fuse on data set
+  const fuse = new Fuse(markers);
+  return fuse.search(searchTerm);
+}
+
+export async function filterMarkers(markers: PageProps["data"]["markers"], searchTerm?: string) {
+  // Await markers from promise data
+  const markerData = await markers;
+  // Null Guard in case no data
+  if(!markerData) return null;
+  // Retrieve potential before and after operators
+  const beforeOp = page.url.searchParams.get("before");
+  const afterOp = page.url.searchParams.get("after");
+  let markerList = markerData;
+  if(beforeOp || afterOp) markerList = filterMarkersByDate(markerData) 
+  if(searchTerm && markerList) markerList = filterMarkersByTerm(markerList, searchTerm)
+  return markerList;
+} 
