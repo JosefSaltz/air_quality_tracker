a7f51f8fd789cc70d06c0378da60f4dcefa6f75c
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-29 08:56:01 -0700

    new(SearchTag): Prototype of date delimiting tags

---
+++ src/lib/components/Search/SearchTag.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Temporal } from "@js-temporal/polyfill";
+  type Props = { tagPrefix: string, date?: string, type?: string };
+  let { tagPrefix, date, type }: Props = $props();
+  const dateTime = new Date(date);
+
+</script>
+
+<div class="text-sm m-2 py-1 px-2 bg-fuchsia-200 rounded-sm ">
+  {#if type === 'time'}
+    <span>{ tagPrefix }: { date ? dateTime.toLocaleString("en-US", { "timeStyle": "short", "dateStyle": "medium" }) : null }</span>
+  {/if}
+  
+</div>
