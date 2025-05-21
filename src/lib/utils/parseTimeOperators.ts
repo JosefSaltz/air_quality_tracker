6c1b32426dea730ed1f326047a12196fea98ecbd
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-21 14:59:04 -0700

    update(parseTimeOperators): Update to use Svelte page API and Temporal API

--- src/lib/utils/parseTimeOperators.ts
+++ src/lib/utils/parseTimeOperators.ts
@@ -1,16 +1,15 @@
-
+import { page } from "$app/state";
+import { Temporal } from '@js-temporal/polyfill';
 
 // Extracts the date from searched before: and after: operators in YYYY-MM-DD format
-export const parseTimeOperators = (searchInput?: string | null) => {
-  // Invalid input returns the standard output with null values
-  if(!searchInput) return { afterDate: null, beforeDate: null };
-  // Pattern matches for determining if operators in input
-  const beforeMatch = searchInput.match(/before:(\d{4}-\d{2}-\d{2})/);
-  const afterMatch = searchInput.match(/after:(\d{4}-\d{2}-\d{2})/);
-  // Assign matches
-  const beforeDate = beforeMatch ? beforeMatch[1] : null;
-  const afterDate = afterMatch ? afterMatch[1] : null;
-  // Return object matched pattern or null
-  return { afterDate, beforeDate };
-}
+export const parseTimeOperators = () => {
+  // QOL Assign the URL Params
+  const params = page.url.searchParams;
+  // Assign required parameters
+  const [beforeParam, afterParam] = [params.get('before'), params.get('after')];
+  // Conditionally assign missing params and default to today if not found
+  const after = afterParam ? Temporal.PlainDate.from(afterParam) : Temporal.Now.plainDateISO();
+  const before = beforeParam ? Temporal.PlainDate.from(beforeParam) : Temporal.Now.plainDateISO(); 
+  return { before, after }
+} 
 
