4db446e882d070b8f0ef6cd1f36dff87a257d3d2
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-21 21:10:21 -0700

    cleanup(filterMarkers): Clean up type def, add All mode whitelist

--- src/lib/utils/filterMarkers.ts
+++ src/lib/utils/filterMarkers.ts
@@ -1,13 +1,15 @@
 import { page } from "$app/state";
 import type { PageProps } from "../../routes/$types";
 import { Temporal } from '@js-temporal/polyfill';
-import Fuse, { type FuseResult, type FuseSearchOptions } from 'fuse.js';
+import Fuse from 'fuse.js';
 import { browser } from "$app/environment";
 import { isOnAfter, isOnBefore } from "./timeCompare";
 import type { TimeOptionKey } from "../constants";
 import { parseTimeOperators } from "./parseTimeOperators";
 
-function filterMarkersByDate(markers: PageProps["data"]["markers"], time: TimeOptionKey = 'Month') { 
+type Markers = PageProps["data"]["markers"];
+
+function filterMarkersByDate(markers: Markers, time: TimeOptionKey = 'Month') { 
   console.log('Filtering By Date');
   // Null Guard
   if(!markers) return markers;
@@ -15,7 +17,7 @@ function filterMarkersByDate(markers: PageProps["data"]["markers"], time: TimeOp
   const { before, after } = parseTimeOperators();
   // Returns a filtered marker list between the supplier date range
   const { compare } = Temporal.PlainDate;
-  // Filter the list
+  // Return the list filtered inclusively between the after and before dates
   return markers.filter(marker => {
     // Get an instant of the marker's date
     const markerDate = Temporal.PlainDate.from(marker.created_at)
@@ -26,7 +28,7 @@ function filterMarkersByDate(markers: PageProps["data"]["markers"], time: TimeOp
   })
 }
 
-function filterMarkersByTerm(markers: ReturnType<typeof filterMarkersByDate>, searchTerm?: string) {
+function filterMarkersByTerm(markers: ReturnType<typeof filterMarkersByDate> | Markers, searchTerm?: string) {
   console.log('Filtering By Term')
   if(!searchTerm || !markers) return markers;
   const options = {
@@ -37,22 +39,23 @@ function filterMarkersByTerm(markers: ReturnType<typeof filterMarkersByDate>, se
   return fuse.search(searchTerm).map(result => result.item);
 }
 
-export function filterMarkers(markers: PageProps["data"]["markers"], searchTerm?: string | null) {
+export function filterMarkers(markers: Markers, searchTerm?: string | null) {
   console.log('Filtering markers:', markers)
-  if(!browser) return null;
-  // Null Guard in case no data
-  if(!markers) return null;
+  // Null guard for now browser or marker data
+  if(!browser || !markers) return null;
   // QOL Assign params
   const params = page.url.searchParams;
-  // Return all markers if time param is 'All'
-  if(params.get('time') === "All") return markers;
   // Retrieve potential before and after operators
-  const beforeOp = params.get("before");
-  const afterOp = params.get("after");
-  const time: TimeOptionKey = params.get('time') as TimeOptionKey | undefined || 'Month';
+  const [beforeParam, afterParam, timeParam] = [
+    params.get("before"), 
+    params.get("after"), 
+    params.get('time') as TimeOptionKey | undefined || 'Month'
+  ];
+  const hasTimeParams = beforeParam && afterParam ? true : false;
   // Assign and return a new reference of the post-processed data
   let markerList: typeof markers | null = markers;
-  if(beforeOp && afterOp) markerList = filterMarkersByDate(markers, time);
-  if(searchTerm && markerList) markerList = filterMarkersByTerm(markerList, searchTerm)
+  // Filter by date unless the url time param is set to All
+  if(hasTimeParams && timeParam !== 'All') markerList = filterMarkersByDate(markers, timeParam);
+  if(searchTerm && markerList) markerList = filterMarkersByTerm(markerList, searchTerm);
   return markerList;
 } 
