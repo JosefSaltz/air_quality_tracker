b167e8337233c3b8bfba39ec63cfbd6a28a98dd5
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-06-08 12:37:51 -0700

    add(profile-settings): Boilerplate new profile settings page

---
+++ src/lib/components/Profile/ExportData/ExportData.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Button from "$components/ui/button/button.svelte";
+  let { handleClick } = $props();
+</script>
+
+<div id="data-exports">
+  <h2 class="text-xl">Request Report Data</h2>
+  <div class="flex flex-col gap-2">
+    <label for="download-reports">Download Reports as a CSV file</label>
+    <Button class="max-w-24" name="download-reports" onclick={handleClick}>Download</Button>
+  </div>
+</div>
---
+++ src/routes/auth/profile/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import Button from "$components/ui/button/button.svelte";
+  import type { PageProps } from "./$types"
+  import PersonalInfo from "$components/Profile/PersonalInfo/PersonalInfo.svelte";
+  import ExportData from "$components/Profile/ExportData/ExportData.svelte";
+  let { data }: PageProps  = $props();
+  const { first_name, last_name, email } = data.profile;
+  const handleDataDownload = () => {
+
+  }
+
+</script>
+
+<div class="p-8">
+  <h1 class="text-3xl md:text-5xl mb-8">Profile</h1>
+  <form use:enhance method="POST" class="space-y-4">
+    <PersonalInfo { first_name } { last_name } { email }/>
+    <ExportData handleClick={handleDataDownload} />
+    <div id="submit-changes" class="flex flex-col gap-2">
+      <label for="submit">Apply Settings</label>
+      <Button name="submit" type="submit" class="max-w-24">Update</Button>
+    </div>
+  </form>
+</div>
