f98998f7f9a870d08ce230fd8347b46311f45e8e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-06-15 00:25:59 -0700

    add(AdminExportDate): Component form of the admin export data button section

---
+++ src/lib/components/Profile/AdminExportData/AdminExportData.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Button from "$components/ui/button/button.svelte";
+  import Input from "$components/ui/input/input.svelte";
+</script>
+
+<div id="data-exports">
+  <h2 class="text-xl">Admin Request Report Data</h2>
+  <div class="flex flex-col gap-2">
+    <label for="reports-password">Please set a password for the admin data download</label>
+    <Input type="text" name="reports-password" required />
+    <label for="download-reports">Download Reports as an encrypted CSV file</label>
+    <Button class="max-w-24" name="download-reports" formaction="?/admin-request-reports">Download</Button>
+  </div>
+</div>
