494c7eccfc09847e2aa66d720af930bfbdfa9634
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-06-18 19:42:02 -0700

    update(profile): Update profile page with better styles

--- src/routes/auth/profile/+page.svelte
+++ src/routes/auth/profile/+page.svelte
@@ -2,26 +2,30 @@
   import { enhance } from "$app/forms";
   import Button from "$components/ui/button/button.svelte";
   import type { PageProps } from "./$types"
-  import PersonalInfo from "$components/Profile/PersonalInfo/PersonalInfo.svelte";
-  import ExportData from "$components/Profile/ExportData/ExportData.svelte";
-  import AdminExportData from "$components/Profile/AdminExportData/AdminExportData.svelte";
-  let { data }: PageProps  = $props();
+  import PersonalInfo from "@/lib/components/Profile/PersonalInfo.svelte";
+  import ExportData from "@/lib/components/Profile/ExportData.svelte";
+  import AdminExportData from "@/lib/components/Profile/AdminExportData.svelte";
+  import type { SubmitFunction } from "@sveltejs/kit";
+  let { data, form }: PageProps  = $props();
+
   let [first_name, last_name, email ]: string[] | null[] = ['', '', '']
   if(data.profile) ({ first_name, last_name, email } = data.profile);
-
+  const handleSubmit: SubmitFunction = ({}) => {}
 </script>
 
-<div class="w-full flex flex-col items-center p-8">
-  <h1 class="text-3xl md:text-5xl mb-8">Profile</h1>
-  <form use:enhance method="POST" class="space-y-4">
-    <PersonalInfo { first_name } { last_name } { email }/>
-    <ExportData />
-    {#if data.user?.role === "admin"}
-      <AdminExportData />
-    {/if}
-    <div id="submit-changes" class="flex flex-col gap-2">
-      <label for="submit">Apply Settings</label>
-      <Button name="submit" type="submit" class="max-w-24" formaction="?/apply-settings">Update</Button>
+<div id="profile-container" class="w-full flex flex-col items-center">
+  <div id="form-container" class="w-full max-w-xl p-4 md:p-8">
+    <h1 class="w-full text-5xl py-16">Profile</h1>
+    <form use:enhance={handleSubmit} method="POST" class="w-full space-y-4">
+      <PersonalInfo { first_name } { last_name } { email }/>
+      <ExportData />
+      {#if data.user?.role === "admin"}
+        <AdminExportData />
+      {/if}
+      <div id="submit-changes" class="flex flex-col gap-2">
+        <label for="submit">Apply Settings</label>
+        <Button name="submit" type="submit" class="max-w-24" formaction="?/apply-settings">Save</Button>
+      </div>
+    </form>
     </div>
-  </form>
 </div>
