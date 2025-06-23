e6c475cc0d774f3bb3afedddef66797a0c0dd157
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-06-22 21:26:00 -0700

    refactor(PasswordInputs): Extrapolate the sign up email inputs to their own component

---
+++ src/lib/components/Forms/signup/PasswordInputs.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let { password = $bindable(), confirm_pw = $bindable()} = $props();
+</script>
+
+<div id="password-container">
+  <!-- Password Input -->
+  <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+  <div class="mt-2">
+    <input type="password" name="password" bind:value={password} id="password" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+  </div>
+  <!-- Password Confirm -->
+  <label for="confirm" class="block text-sm/6 font-medium text-gray-900">Verify Password</label>
+  <div class="mt-2">
+    <input type="password" name="confirm" bind:value={confirm_pw} id="confirm" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
+  </div>
+</div>
--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -7,6 +7,7 @@
   import { type SubmitFunction } from "@sveltejs/kit";
   import { validPasswordSchema } from "@/lib/utils/zSchemas/validPasswordSchema";
   import LogoPiita from "$components/LogoPiita.svelte";
+  import PasswordInputs from "@/lib/components/Forms/signup/PasswordInputs.svelte";
 
   const emailSchema = z.string().email();
   let { data }: PageProps = $props();
@@ -61,18 +62,7 @@
           {/if}
         </div>
         <!-- Password Inputs -->
-        <div id="password-container">
-          <!-- Password Input -->
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-          <div class="mt-2">
-            <input type="password" name="password" bind:value={password} id="password" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
-          </div>
-          <!-- Password Confirm -->
-          <label for="confirm" class="block text-sm/6 font-medium text-gray-900">Verify Password</label>
-          <div class="mt-2">
-            <input type="password" name="confirm" bind:value={confirm_pw} id="confirm" autocomplete="current-password" minlength=6 required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline--600 sm:text-sm/6">
-          </div>
-        </div>
+        <PasswordInputs bind:password bind:confirm_pw />
         <!-- Dynamic Validation Feedback -->
         {#if password && confirm_pw}
           {#if verified_pw}
