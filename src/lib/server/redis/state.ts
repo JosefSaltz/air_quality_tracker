091e336e42b832088f94da1cac07b8c6230e701f
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-20 23:19:35 -0700

    refactor(state): Refactor redis state

---
+++ src/lib/server/redis/state.ts
@@ -0,0 +1,14 @@
+type RedisState = {
+  redisFailed?: boolean,
+  redisOnline?: boolean
+}
+// Redis Status Flag
+let _redisOnline = false;
+let _redisFailed = false;
+// State Getter
+export const getRedisStatus = () => ({ redisOnline: _redisOnline, redisFailed: _redisFailed });
+// State Setter
+export const setRedisStatus = ({redisFailed, redisOnline}: RedisState) => { 
+  if(typeof redisFailed === "boolean") _redisFailed = redisFailed; 
+  if(typeof redisOnline === "boolean") _redisOnline = redisOnline 
+} 
