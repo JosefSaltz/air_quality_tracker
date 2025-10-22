a87afa07325fd8176baacf34f9986968afef3b86
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-10-21 21:49:19 -0700

    add(page): Add expired verification link page

---
+++ src/routes/auth/expired-verification/+page.svelte
@@ -0,0 +1,4 @@
+<div id="error-container" class="w-full h-full flex flex-col justify-center items-center">
+  <p class="text-3xl">⚠️ Verification Link Expired</p>
+  <p>Your verification link has expired, you can request another through our <a href="/support">Support Page</a></p>
+</div>
