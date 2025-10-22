26b18da492af298b0dddf11a8baceb86bd8c8017
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-10-21 22:10:39 -0700

    update(page): Tweak styles for expired link page

--- src/routes/auth/expired-verification/+page.svelte
+++ src/routes/auth/expired-verification/+page.svelte
@@ -1,4 +1,4 @@
-<div id="error-container" class="w-full h-full flex flex-col justify-center items-center">
+<div id="error-container" class="w-full h-full flex flex-col justify-center items-center gap-3">
   <p class="text-3xl">⚠️ Verification Link Expired</p>
-  <p>Your verification link has expired, you can request another through our <a href="/support">Support Page</a></p>
+  <p class="text-xl">Your verification link has expired, you can request another through our <a href="/support" class="support-link hover:text-sky-600 text-sky-800 underline">Support Page</a></p>
 </div>
