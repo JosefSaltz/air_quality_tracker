2656fde51f901fdbbe4107a14f3e5a6c0d288f44
Josef Saltz <JosefSaltz@users.noreply.github.com>
2026-01-10 19:53:02 -0800

    update(createExcel): Implement the new unique login data structure in createExcelFile

--- src/lib/server/createExcel.ts
+++ src/lib/server/createExcel.ts
@@ -3,8 +3,10 @@ import officeCrypto from "officecrypto-tool";
 import { type ReportData } from "@/routes/api/download/reports/+server";
 // Utility function for preparing an Excel file to send to the user
 export const createExcelFile = async (reportData: ReportData, password?: string) => {
+  if(!reportData) return;
+  const { markerReports, uniqueLogins } = reportData;
   // Null Guard
-  if(!reportData || !reportData.length) return;
+  if(!markerReports || !markerReports.length) return;
   // Instantiate new Workbook
   const workbook = new Excel.Workbook();
   // Assign Workbook Metadata
@@ -12,12 +14,15 @@ export const createExcelFile = async (reportData: ReportData, password?: string)
   workbook.created = new Date();
   // Create the worksheet with report data
   const worksheet = workbook.addWorksheet('Report Data');
+  const markerHeaders = Object.keys(markerReports[0]);
   // Create column headers
-  const headers = Object.keys(reportData[0]);
+  const headers = uniqueLogins ? [ ...markerHeaders, "Unique Logins"] : markerHeaders;
   const extrapolatedColumns = headers.map(header => ({ header, key: header}));
   worksheet.columns = extrapolatedColumns;
-  // Add all the data rows
-  reportData.forEach(report => worksheet.addRow(report));
+  // Prepend a row of the unique login count
+  if(uniqueLogins) worksheet.addRow({ "Unique Logins": uniqueLogins})
+  // Add all the marker data rows
+  markerReports.forEach(report => worksheet.addRow(report));
   // Generate the buffer for the Excel file
   const workbookBuffer = await workbook.xlsx.writeBuffer();
   // Return an encrypted copy if there's a password present
