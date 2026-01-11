d040aed1fbf2e9364c66040a7c629cae76edf28b
Josef Saltz <JosefSaltz@users.noreply.github.com>
2026-01-10 19:32:24 -0800

    update(getAdminReports): Admin reports now returns structured data with uniqueLogins

--- src/lib/server/getAdminReports.ts
+++ src/lib/server/getAdminReports.ts
@@ -1,9 +1,8 @@
 import type { retrieveDateParams } from "./retrieveDateParams";
 
-
+import { getUniqueLogins } from "./getUniqueLogins";
 export async function getAdminReports(supabase: App.Locals["supabase"], dateRange: ReturnType<typeof retrieveDateParams>) {
   const { start_date, end_date } = dateRange;
- 
   const markers = await supabase
     .from("reports")
     .select("id, created_at, description, humidity, latitude, longitude, strength, temperature_f, wind_direction, wind_speed_kn, profiles ( email )")
@@ -16,14 +15,16 @@ export async function getAdminReports(supabase: App.Locals["supabase"], dateRang
     console.error(`❌ Something went wrong trying to retrieve marker data`, error.message);
     return null;
   }
+  // Retrieve the count of the unique logins we had since the 1st of the month
+  const { uniqueLogins } = await getUniqueLogins();
   // Escape early if there isn't any records returned
   if(!markers.data.length) return markers.data;
 
-  const formattedData = markers.data.map(record => {
+  const markerReports = markers.data.map(record => {
     const { profiles, ...restOfRecord } = record;
     const { email } = profiles;
     return { ...restOfRecord, email };
   })
 
-  return formattedData;
+  return { markerReports, uniqueLogins };
 }
